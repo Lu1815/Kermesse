8899930fe5de6baed323fb234d42bead26b276a0
LuisFCE <luisfelipece17@gmail.com>
2021-10-18 14:28:44 -0600

    Reporte agregado, falta quitar el campo estado y modificar el Insert, edit y delete del controlador

--- Kermesse/Controllers/MonedasController.cs
+++ Kermesse/Controllers/MonedasController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Kermesse.Models;
+using Microsoft.Reporting.WebForms;
 
 namespace Kermesse.Controllers
 {
@@ -124,6 +126,26 @@ namespace Kermesse.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult verReporte(string tipo)
+        {
+            LocalReport rpt = new LocalReport();
+            string mt, enc, f;
+            string[] s;
+            Warning[] w;
+
+            string ruta = Path.Combine(Server.MapPath("~/Reports"),"RptMoneda.rdlc");
+            rpt.ReportPath = ruta;
+
+            List<Moneda> ls = new List<Moneda>();
+            ls = db.Monedas.ToList();
+
+            ReportDataSource rd = new ReportDataSource("DSMoneda", ls);
+            rpt.DataSources.Add(rd);
+
+            var b = rpt.Render(tipo, null, out mt, out enc, out f, out s, out w);
+            return new FileContentResult(b, mt);
         }
     }
 }
